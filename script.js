66ad9973605e912a79e002efea7eb8ad2648aa03
James Hancock <jhancock532@gmail.com>
2021-03-05 22:46:37 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,152 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import Stats from 'stats.js';
+ 
+/* Stats.js */
+var stats = new Stats();
+stats.showPanel(0);
+document.body.appendChild(stats.dom);
+
+/* Three.js */
+const canvasContainer = document.getElementById( 'canvas-container' );
+const scene = new THREE.Scene();
+scene.background = new THREE.Color( 0xcce0ff );
+//scene.fog = new THREE.Fog( 0xcce0ff, 500, 10000 );
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 5;
+camera.position.y = 3;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.shadowMap.enabled = true;
+renderer.gammaOutput = true;
+//renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+canvasContainer.appendChild( renderer.domElement );
+
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.maxPolarAngle = Math.PI * 0.45;
+controls.minDistance = 5;
+controls.maxDistance = 20;
+
+const color = 0xFFFFFF;
+const intensity = 0.2;
+const light = new THREE.AmbientLight(color, intensity);
+scene.add(light);
+
+const sunColor = 0xFF8833;
+const sunIntensity = 1.9;
+const sunLight = new THREE.DirectionalLight(sunColor, sunIntensity);
+sunLight.position.set(-50, 40, 10);
+sunLight.target.position.set(0, 0, 0);
+
+sunLight.castShadow = true; 
+
+sunLight.shadow.mapSize.width = 8192; 
+sunLight.shadow.mapSize.height = 8192; 
+sunLight.shadow.camera.left = -100;
+sunLight.shadow.camera.bottom = -100;
+sunLight.shadow.camera.right = 100;
+sunLight.shadow.camera.top = 100;
+
+const helper = new THREE.CameraHelper( sunLight.shadow.camera );
+scene.add( helper );
+scene.add(sunLight);
+scene.add(sunLight.target);
+
+//const clock = new THREE.Clock();
+
+//Create a sphere that cast shadows 
+const sphereGeometry = new THREE.SphereGeometry( 1, 32, 32 );
+const sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
+const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+sphere.translateY(7);
+sphere.castShadow = true; 
+sphere.receiveShadow = false;
+scene.add( sphere );
+
+const planeGeometry = new THREE.PlaneGeometry( 20, 20, 32, 32 );
+const planeMaterial = new THREE.MeshStandardMaterial( { color: 0x00ff00 } )
+const plane = new THREE.Mesh( planeGeometry, planeMaterial );
+plane.rotateY(-Math.PI / 2);
+plane.translateZ(-8.5);
+plane.receiveShadow = true;
+scene.add( plane );
+
+/* Model Loading */
+const loader = new GLTFLoader();
+
+loader.load( 'models/CubeHouseDemo.glb',
+  //This doesn't recieve shadows :/
+  //CubeHouseExported.gltf does! But the textures are messed up by blender. :(
+  //The key difference is that the .gltf loads with 
+	function ( gltf ) {
+		
+    gltf.scene.traverse( function ( child ) {
+      //And awkward update of encoding is required to load the Image Based Lighting correctly.
+      if ( child.isMesh ) {
+        child.castShadow = true;
+        child.receiveShadow = true;
+        child.material.roughness = 1;
+        child.material.shadowSide = THREE.DoubleSide;
+        child.material.side = THREE.DoubleSide;
+        //child.material.isGLTFSpecularGlossinessMaterial = false;
+
+      }
+    });
+
+    scene.add( gltf.scene );
+	},
+	function ( xhr ) {
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+	},
+	function ( error ) {
+		console.log( 'An error happened' );
+	}
+);
+
+/* Resize the Window */
+window.addEventListener('resize', onWindowResize);
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+/* Click to Find Object */
+const raycaster = new THREE.Raycaster();
+
+function onClick( event ) {
+  event.preventDefault();
+  
+  raycaster.setFromCamera( {x:0, y:0}, camera );
+  
+  let intersects = raycaster.intersectObjects( scene.children, true );
+  
+  if ( intersects.length > 0 ) {
+    console.log(intersects[0].object);
+  }
+}
+
+renderer.domElement.addEventListener('click', onClick, false);
+
+/* Animation */
+const animate = function () {
+  stats.begin();
+
+  //var delta = clock.getDelta();
+
+  renderer.render( scene, camera );
+  
+  stats.end();
+
+  requestAnimationFrame( animate );
+};
+
+animate();
+
+
